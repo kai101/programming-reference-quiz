fdebe54b29ceaead6cd940608506285f88a5aca0
Leon <leonyapkl@gmail.com>
2017-03-17 19:06:45 +0800

    selection and insertion sort

---
+++ golang-go/sorting-algorithm/insertion-sort/insertion_sort.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func InsertionSort(arr []int) []int{
+	var min int = 0;
+	var intHolder int;
+	for i := range arr {
+		min = i;
+		
+		for j := i+1 ; j < len(arr) ; j ++ {
+			if(arr[j] < arr[i]){
+				min = j
+			}
+		}
+		
+		//swap
+		if(min != i){
+			intHolder = arr[i]
+			arr[i] = arr[min]
+			arr[min] = intHolder
+		}
+		
+	}
+
+
+	return arr;
+}
+
+func main() {
+	var arr [10]int
+	var slice = arr[:] //make a slice #see golang slice and array
+	
+	//setup pseudorandom number generator
+	source := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(source)
+	
+	//initialize array with random number of 0 to 100;
+	for index := range arr{
+		arr[index] = random.Intn(100);
+	}
+	
+	//output before sort
+	fmt.Printf("Before: %v\n", arr)
+	
+	//sorting
+	result := InsertionSort(slice);
+	
+	//output after sort
+	fmt.Printf("after:  %v\n", result)
+}
